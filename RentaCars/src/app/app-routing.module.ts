da47701b37c66e8bf34c97d1de401e015810cb35
Pedro Cano <pedrinche27@gmail.com>
2019-03-25 22:28:55 -0600

    Reserva Lista

--- RentaCars/src/app/app-routing.module.ts
+++ RentaCars/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'reserva', loadChildren: './reserva/reserva.module#ReservaPageModule' },
+  { path: 'entrega', loadChildren: './entrega/entrega.module#EntregaPageModule' },
 ];
 
 @NgModule({
